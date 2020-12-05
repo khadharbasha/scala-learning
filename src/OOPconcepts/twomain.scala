6cfa7f7e73f4b58a2580853f0bf7913ad1130874
khadharbasha <bashaasjack@gmail.com>
2020-12-05 13:19:08 +0530

    OOPs

--- src/OOPconcepts/twomain.scala
+++ src/OOPconcepts/twomain.scala
@@ -2,10 +2,10 @@ package OOPconcepts
 
 object twomain {
   def add(x:Int, y:Int): Int = x + y
-  def sub(x:Int, y:Int): Int = return (x - y)
+  def sub(x:Int, y:Int): Int =  x - y
 }
 
-  object nextobj {
+  object extobj {
     def main(args: Array[String]): Unit = {
       println(twomain.add(5,6))
       println(twomain.sub(9,6))
